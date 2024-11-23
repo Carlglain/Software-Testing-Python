28e9b155212e3a03dd746eeede0614e6076df63b
Carlstorm <carlglainnyuysemo@gmail.com>
2024-11-23 05:10:59 +0100

    error 404 for continues intergration

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify
 app = Flask(__name__) ;
 
 
-@app.route('/')
+@app.route('/Path_Test') #this is a random path just for continues testing
 def home():
     return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
